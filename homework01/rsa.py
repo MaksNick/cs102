28366c8167ea858be87adb27fcf041272e91517b
MaksNick <nic50@list.ru>
2021-10-02 22:29:28 +0300

    Указание аннотации для массива

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -59,7 +59,7 @@ def multiplicative_inverse(e: int, phi: int) -> int:
     >>> multiplicative_inverse(7, 40)
     23
     """
-    delen: Iterable[int] = []
+    delen: list = []
     phi1 = phi
     delen.insert(0, phi // e)
     while phi % e != 0:

3331746d9ce1c2b3461eb9a26629ef6ebca6a9f5
Максим Никоноров <53188863+MaksNick@users.noreply.github.com>
2021-10-30 13:52:59 +0300

    Revert "Реализованы все функции кроме caesar_breaker)"

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -14,21 +14,9 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     >>> encrypt_caesar("")
     ''
     """
-    ciphertext = []
-    for i in range(len(plaintext)):
-        if ord(plaintext[i]) >= 65 and ord(plaintext[i]) <= 90:
-            b = chr(ord(plaintext[i]) + shift % 28)
-            if ord(b) > 90:
-                b = chr(ord(b) - 26)
-            ciphertext.append(b)
-        elif ord(plaintext[i]) >= 97 and ord(plaintext[i]) <= 122:
-            b = chr(ord(plaintext[i]) + shift % 28)
-            if ord(b) > 122:
-                b = chr(ord(b) - 26)
-            ciphertext.append(b)
-        else:
-            ciphertext.append(plaintext[i])
-    return "".join([str(i) for i in ciphertext])
+    ciphertext = ""
+    # PUT YOUR CODE HERE
+    return ciphertext
 
 
 def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
@@ -44,21 +32,9 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     >>> decrypt_caesar("")
     ''
     """
-    plaintext = []
-    for i in range(len(ciphertext)):
-        if ord(ciphertext[i]) >= 65 and ord(ciphertext[i]) <= 90:
-            b = chr(ord(ciphertext[i]) - shift % 28)
-            if ord(b) < 65:
-                b = chr(ord(b) + 26)
-            plaintext.append(b)
-        elif ord(ciphertext[i]) >= 97 and ord(ciphertext[i]) <= 122:
-            b = chr(ord(ciphertext[i]) - shift % 28)
-            if ord(b) < 97:
-                b = chr(ord(b) + 26)
-            plaintext.append(b)
-        else:
-            plaintext.append(ciphertext[i])
-    return "".join([str(i) for i in plaintext])
+    plaintext = ""
+    # PUT YOUR CODE HERE
+    return plaintext
 
 
 def caesar_breaker_brute_force(ciphertext: str, dictionary: tp.Set[str]) -> int:
--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -9,32 +9,9 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     >>> encrypt_vigenere("ATTACKATDAWN", "LEMON")
     'LXFOPVEFRNHR'
     """
-    ciphertext = []
-    for i in range(len(plaintext)):
-        if i > len(keyword) - 1:
-            if ord(keyword[(i) % len(keyword)]) >= 65 and ord(keyword[(i) % len(keyword)]) <= 90:
-                shift = ord(keyword[(i) % len(keyword)]) - ord("A")
-            else:
-                shift = ord(keyword[(i) % len(keyword)]) - ord("a")
-        else:
-            if ord(keyword[i]) >= 65 and ord(keyword[i]) <= 90:
-                shift = ord(keyword[i]) - ord("A")
-            else:
-                shift = ord(keyword[i]) - ord("a")
-
-        if ord(plaintext[i]) >= 65 and ord(plaintext[i]) <= 90:
-            b = chr(ord(plaintext[i]) + shift)
-            if ord(b) > 90:
-                b = chr(ord(b) - 26)
-            ciphertext.append(b)
-        elif ord(plaintext[i]) >= 97 and ord(plaintext[i]) <= 122:
-            b = chr(ord(plaintext[i]) + shift)
-            if ord(b) > 122:
-                b = chr(ord(b) - 26)
-            ciphertext.append(b)
-        else:
-            ciphertext.append(plaintext[i])
-    return "".join([str(i) for i in ciphertext])
+    ciphertext = ""
+    # PUT YOUR CODE HERE
+    return ciphertext
 
 
 def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
@@ -48,29 +25,6 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     >>> decrypt_vigenere("LXFOPVEFRNHR", "LEMON")
     'ATTACKATDAWN'
     """
-    plaintext = []
-    for i in range(len(ciphertext)):
-        if i > len(keyword) - 1:
-            if ord(keyword[(i) % len(keyword)]) >= 65 and ord(keyword[(i) % len(keyword)]) <= 90:
-                shift = ord(keyword[(i) % len(keyword)]) - ord("A")
-            else:
-                shift = ord(keyword[(i) % len(keyword)]) - ord("a")
-        else:
-            if ord(keyword[i]) >= 65 and ord(keyword[i]) <= 90:
-                shift = ord(keyword[i]) - ord("A")
-            else:
-                shift = ord(keyword[i]) - ord("a")
-
-        if ord(ciphertext[i]) >= 65 and ord(ciphertext[i]) <= 90:
-            b = chr(ord(ciphertext[i]) - shift)
-            if ord(b) < 65:
-                b = chr(ord(b) + 26)
-            plaintext.append(b)
-        elif ord(ciphertext[i]) >= 97 and ord(ciphertext[i]) <= 122:
-            b = chr(ord(ciphertext[i]) - shift)
-            if ord(b) < 97:
-                b = chr(ord(b) + 26)
-            plaintext.append(b)
-        else:
-            plaintext.append(ciphertext[i])
-    return "".join([str(i) for i in plaintext])
+    plaintext = ""
+    # PUT YOUR CODE HERE
+    return plaintext
